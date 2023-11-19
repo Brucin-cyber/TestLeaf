c0d992bc1816e03f6d9133024c9a3fcea47daa79
Brucin-cyber <68371483+Brucin-cyber@users.noreply.github.com>
2023-11-19 23:19:34 +0530

    Add files via upload

---
+++ Bike.java
@@ -0,0 +1,12 @@
+package weekday.day1;
+
+public class Bike {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car test=new Car();
+		test.applyBreak();
+		test.soundHorn();
+	}
+
+}
