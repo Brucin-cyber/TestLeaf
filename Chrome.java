e3e010901c14cde0304e3848ac6f96b01d74c49c
Brucin-cyber <68371483+Brucin-cyber@users.noreply.github.com>
2023-11-19 20:34:12 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,13 @@
+package weekday.day1;
+
+public class Chrome {
+	public void getName()
+	{
+		System.out.println("This is Chrome");
+	}
+	public void printName()
+	{
+		System.out.println("This is Google Chrome");
+	}
+	
+}
---
+++ ChromeBrowser.java
@@ -0,0 +1,12 @@
+package weekday.day1;
+
+public class ChromeBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Chrome browser=new Chrome();
+		browser.getName();
+		browser.printName();
+	}
+
+}
