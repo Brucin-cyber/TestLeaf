f3cb64eb0d15ea4470c9fbf94876827cfa494930
Brucin-cyber <68371483+Brucin-cyber@users.noreply.github.com>
2023-11-22 22:26:58 +0530

    Add files via upload

---
+++ PositiveNegative.java
@@ -0,0 +1,22 @@
+package weekday.day2;
+
+public class PositiveNegative {
+
+	public static void main(String[] args) {
+		int num = 35;
+		if(num>0)
+		{
+			System.out.println("Entered Number is Positive");
+		}
+		else if(num<0)
+		{
+			System.out.println("Entered Number is Negative");
+		}
+		else
+		{
+			System.out.println("Entered Number is Null");
+		}
+
+	}
+
+}
