27bd85e5dd13e66f4a50db66806da76eb2aecc65
Brucin-cyber <68371483+Brucin-cyber@users.noreply.github.com>
2023-11-22 22:48:22 +0530

    Add files via upload

---
+++ ConvertPositive.java
@@ -0,0 +1,16 @@
+package weekday.day2;
+
+public class ConvertPositive {
+
+	public static void main(String[] args) {
+		int number = -40;
+		if(number<0)
+		{
+			System.out.println("Its a Negative Number");
+		}
+		int positive = number *(-1);
+		System.out.println("Converted to positive number: "+positive);
+
+	}
+
+}
